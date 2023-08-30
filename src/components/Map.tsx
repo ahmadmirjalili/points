a6e3be887e0048aec7891ea817ef68b85686533e
Seyed Ahmad Mirjalili <s.mirjalili95@gmail.com>
2023-08-30 19:13:41 +0330

    add and config map in  zustand context

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -2,15 +2,20 @@ import L from "leaflet";
 import { useEffect } from "react";
 import "leaflet/dist/leaflet.css";
 import styles from "src/assets/styles/components/Map.module.css";
+import { useMapStore } from "src/services/hooks/zustand/map/useMapStore";
 
 const Map = () => {
+  const setMap = useMapStore((state) => state.setMap);
+
   useEffect(() => {
-    let map = L.map("map").setView([33.43406047459431, 52.69050235306594], 6);
+    const map = L.map("map").setView([33.43406047459431, 52.69050235306594], 6);
 
     L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
+
+    setMap(map);
   }, []);
 
   return (
